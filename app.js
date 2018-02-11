672c081bfdbd0dd7fa51292ae0e60905519507d8
Shubham Rajput <shubhamrajput025@gmail.com>
2018-02-11 16:13:36 +0530

    User registration
    Registration feature ,mlab database

--- app.js
+++ app.js
@@ -1,68 +1,134 @@
 const express = require('express');
 const MoltinGateway = require('@moltin/sdk').gateway;
 const keys = require('./keys');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const app = express();
 
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 const Moltin = MoltinGateway({
-  client_id: keys.molten.client_id,
-  client_secret:keys.molten.client_secret,
+    client_id: keys.molten.client_id,
+    client_secret: keys.molten.client_secret,
 });
 
 
 
 Moltin.Authenticate().then((response) => {
-  console.log('authenticated');
+    console.log('authenticated');
 });
 
+//Middleware
 
-app.get('/',(req,res)=>{
+const cartLength = (req,res,next)=>{
+	 Moltin.Cart().Items().then((items) => {
+        console.log(items);
+        if(items.data.length>0){
+        	next();
+        }
+        else{
+        	res.redirect('/');
+        }
+    });
 
-	const products = Moltin.Products.All().then((products) => {
-		res.render('index',{products:products.data});
-});
 	
+};
+
+
+
+
+app.get('/', (req, res) => {
+
+    const products = Moltin.Products.All().then((products) => {
+    	console.log(products.data[0].price[0].amount);
+        res.render('index', {
+            products: products.data
+        });
+    });
+
 });
 
-app.get('/cart',(req,res)=>{
-console.log(req.query);
-	Moltin.Cart().AddProduct(req.query.id, 1).then((item) => {
-		res.redirect('/');
-});
+app.get('/cart', (req, res) => {
+    console.log(req.query);
+    Moltin.Cart().AddProduct(req.query.id, 1).then((item) => {
+        res.redirect('/');
+    });
 
 
 });
 
 
-app.get('/cartItems',(req,res)=>{
-	const cart = Moltin.Cart().Items().then((items)=>{
-		console.log(items);
-		res.render('cartitems',{items:items.data});
+app.get('/cartItems',cartLength, (req, res) => {
+    const cart = Moltin.Cart().Items().then((items) => {
+        console.log(items);
+        res.render('cartitems', {
+            items: items.data
+        });
+    });
+
+
+
+});
+
+app.get('/remove', (req, res) => {
+    console.log(req.query.id);
+    Moltin.Cart().RemoveItem(req.query.id).then((cart) => {
+        res.redirect('/cartItems');
+    });
+
+});
+
+app.get('/delete', (req, res) => {
+    Moltin.Cart().Delete().then((cart) => {
+        res.redirect('/');
+    });
+
+});
+
+app.get('/login',(req,res)=>{
+	res.render('loginPage');
+});
+
+app.get('/register',(req,res)=>{
+	res.render('registerPage',{
+		errors:[]
 	});
-	
-	
+});
+
+app.post('/register',(req,res)=>{
+	let errors = [];
+	if(req.body.email===""){
+		errors.push({text:"Enter your Email!"});
+
+	}
+	if(req.body.name===""){
+		errors.push({text:"Enter your Name!"});
+
+	}
+	if(req.body.password===""){
+		errors.push({text:"Enter your Password!"});
+
+	}
+	if(errors.length>0)
+	{
+		res.render('registerPage',{
+			errors:errors
+
+		});
+	}
+	else{
+		res.send("Succesfully registered");
+	}
+
 
 });
 
-app.get('/remove',(req,res)=>{
-	console.log(req.query.id);
-	Moltin.Cart().RemoveItem(req.query.id).then((cart) => {
-		res.redirect('/cartItems');
-});
-
-});
-
-app.get('/delete',(req,res)=>{
-	Moltin.Cart().Delete().then((cart) => {
-		res.redirect('/');
-});
-
-});
-
-app.listen(3000,()=>{
-	console.log("app running on port 3000");
+app.listen(3000, () => {
+    console.log("app running on port 3000");
 
 });
--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 module.exports = {
 	
-	"dburl" : "",
+	"dburl" : "mongodb://<dbuser>:<dbpassword>@ds231568.mlab.com:31568/fakestore",
 	"molten" : {
 		"client_id": 'oKHNhDSxlBDs3TIWm39BsTQgf4FXZ2xEbCqr3Kc5Z3',
 		"client_secret":'VFwDFizZNcDTdf6PSjBgG21FxK2KjuTHuHzvbp5Wog'
